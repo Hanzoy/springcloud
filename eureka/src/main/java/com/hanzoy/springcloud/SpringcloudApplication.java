03c794b07f92ea36550ebda9fb0afd98f5421497
Hanzoy <1002745373@qq.com>
2020-11-02 22:37:31 +0800

    新增子模块

--- eureka/src/main/java/com/hanzoy/springcloud/SpringcloudApplication.java
+++ eureka/src/main/java/com/hanzoy/springcloud/SpringcloudApplication.java
@@ -1,13 +1,21 @@
 package com.hanzoy.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+@Slf4j
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringcloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudApplication.class, args);
+        log.info("启动成功");
     }
 
 }

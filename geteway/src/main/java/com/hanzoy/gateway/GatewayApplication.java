19d20e5c85fd9cb9bd336b6b69aa2e6b220aae8a
Hanzoy <1002745373@qq.com>
2020-11-03 21:44:15 +0800

    新增gateway模块

---
+++ geteway/src/main/java/com/hanzoy/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hanzoy.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        log.info("启动成功");
+    }
+
+}

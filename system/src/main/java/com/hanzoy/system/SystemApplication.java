d3b897bf4e2809b00b746bfdd388da4cc18b2fce
Hanzoy <1002745373@qq.com>
2020-11-06 20:43:31 +0800

    1.将通用配置移动到server中 2.解决了自动注入失败的问题

--- system/src/main/java/com/hanzoy/system/SystemApplication.java
+++ system/src/main/java/com/hanzoy/system/SystemApplication.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan("com.hanzoy.system.mapper")
+@ComponentScan("com.hanzoy")
+@MapperScan("com.hanzoy.server.mapper")
 public class SystemApplication {
 
     public static void main(String[] args) {
--- system/src/main/java/com/hanzoy/system/controller/TestController.java
+++ system/src/main/java/com/hanzoy/system/controller/TestController.java
@@ -1,10 +1,9 @@
 package com.hanzoy.system.controller;
 
-import com.hanzoy.system.domain.Test;
-import com.hanzoy.system.service.TestService;
+import com.hanzoy.server.domain.Test;
+import com.hanzoy.server.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -12,7 +11,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-public class testController {
+public class TestController {
 
     @Resource
     private TestService testService;

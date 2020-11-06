2977119a91936c737b114b719c7a91a18e105ccf
Hanzoy <1002745373@qq.com>
2020-11-06 17:33:01 +0800

    集成热部署DevTool 优化mybatis日志输出

--- system/src/main/java/com/hanzoy/system/controller/testController.java
+++ system/src/main/java/com/hanzoy/system/controller/testController.java
@@ -2,6 +2,7 @@ package com.hanzoy.system.controller;
 
 import com.hanzoy.system.domain.Test;
 import com.hanzoy.system.service.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class testController {
 
@@ -17,6 +19,12 @@ public class testController {
 
     @GetMapping("/test")
     public List<Test> test(){
+        log.info("test");
         return testService.list();
+    }
+
+    @GetMapping("/show")
+    public String show(){
+        return "lslsls";
     }
 }

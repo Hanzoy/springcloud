d94484c14902dd73127cf4119d658d3d95a9bb88
Hanzoy <1002745373@qq.com>
2020-11-11 15:46:36 +0800

    1.增加dto层，用于controller和service层 2.增加ChapterDto 3.修改ChapterService，将Chapter改成ChapterDto

--- business/src/main/java/com/hanzoy/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/hanzoy/business/controller/admin/ChapterController.java
@@ -1,9 +1,11 @@
-package com.hanzoy.business.controller;
+package com.hanzoy.business.controller.admin;
 
 import com.hanzoy.server.domain.Chapter;
+import com.hanzoy.server.dto.ChapterDto;
 import com.hanzoy.server.service.ChapterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -11,19 +13,15 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
     @GetMapping("/chapter")
-    public List<Chapter> chapter(){
+    public List<ChapterDto> chapter(){
         log.info("chapter");
         return chapterService.list();
-    }
-
-    @GetMapping("/chapterShow")
-    public String show(){
-        return "lslsls";
     }
 }

477ce72aef4160c43274e3c88d1b94fd05abad6d
Hanzoy <1002745373@qq.com>
2020-11-11 15:25:44 +0800

    增加大章chapter表查询接口

--- business/src/main/java/com/hanzoy/business/controller/ChapterController.java
+++ business/src/main/java/com/hanzoy/business/controller/ChapterController.java
@@ -1,7 +1,7 @@
 package com.hanzoy.business.controller;
 
-import com.hanzoy.server.domain.Test;
-import com.hanzoy.server.service.TestService;
+import com.hanzoy.server.domain.Chapter;
+import com.hanzoy.server.service.ChapterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +11,18 @@ import java.util.List;
 
 @Slf4j
 @RestController
-public class TestController {
+public class ChapterController {
 
     @Resource
-    private TestService testService;
+    private ChapterService chapterService;
 
-    @GetMapping("/test")
-    public List<Test> test(){
-        log.info("test");
-        return testService.list();
+    @GetMapping("/chapter")
+    public List<Chapter> chapter(){
+        log.info("chapter");
+        return chapterService.list();
     }
 
-    @GetMapping("/testShow")
+    @GetMapping("/chapterShow")
     public String show(){
         return "lslsls";
     }
